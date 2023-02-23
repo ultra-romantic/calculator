1d254049ddf1cb4621563741536fab5678600b5c
ultra-romantic <shiftybroccoli74@gmail.com>
2023-02-23 11:57:32 -0300

    add button value in input screen

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ const btn=document.querySelectorAll('.calculator-button');
 btn.forEach( button => {
     
     const valor = button.getAttribute("data-value");
-    
+    let inputText;
     button.addEventListener('click', e => {
         input.innerHTML = `${valor}`
     })
