1a8745a4c7be6f47639477b1dc34995d7df3b054
ultra-romantic <shiftybroccoli74@gmail.com>
2023-02-23 12:15:47 -0300

    add output gets a value after pressing =

--- javascript.js
+++ javascript.js
@@ -2,12 +2,20 @@ const output = document.getElementById('output');
 const input = document.getElementById('input');
 
 const btn=document.querySelectorAll('.calculator-button');
+const btnResultado = document.getElementById('result');
+
+let valorImprimir=0;
 
 btn.forEach( button => {
-    
     const valor = button.getAttribute("data-value");
-    let inputText;
-    button.addEventListener('click', e => {
-        input.innerHTML = `${valor}`
-    })
-})
+    if(valor!=="="){
+        button.addEventListener('click', e => {
+            input.textContent = `${valor}`
+            valorImprimir=`${valor}`
+        });
+    }
+});
+
+btnResultado.addEventListener('click', e => {
+    output.textContent = valorImprimir;
+});
